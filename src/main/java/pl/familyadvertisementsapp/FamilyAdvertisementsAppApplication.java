f47da655e31bb5972375b04d92093d7d94728ef1
Tomasz Lipka <>
2023-03-03 16:40:37 +0100

    Create spring project

---
+++ src/main/java/pl/familyadvertisementsapp/FamilyAdvertisementsAppApplication.java
@@ -0,0 +1,13 @@
+package pl.familyadvertisementsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyAdvertisementsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyAdvertisementsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/familyadvertisementsapp/FamilyAdvertisementsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.familyadvertisementsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyAdvertisementsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
