b1392e7a937fa7b394a4656f1225bc501b4ebcda
Tomasz Lipka <>
2023-03-06 13:28:28 +0100

    Update spring project

---
+++ src/main/java/pl/familyadvertisementsapp/Test.java
@@ -0,0 +1,13 @@
+package pl.familyadvertisementsapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+    @RequestMapping("/")
+    public String test() {
+        return "Hello World !!!";
+    }
+}
