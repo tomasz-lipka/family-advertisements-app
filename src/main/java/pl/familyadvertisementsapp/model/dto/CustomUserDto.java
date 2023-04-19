874ccf032c64da7ab92297f9fb8bf0c1f6aa14f6
Tomasz Lipka <>
2023-04-19 13:53:35 +0200

    Reenable username and password validation

--- src/main/java/pl/familyadvertisementsapp/model/dto/CustomUserDto.java
+++ src/main/java/pl/familyadvertisementsapp/model/dto/CustomUserDto.java
@@ -2,16 +2,19 @@ package pl.familyadvertisementsapp.model.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import pl.familyadvertisementsapp.validator.ValidPassword;
 
 import java.util.Arrays;
 
 /**
  * Data transfer (form transfer) class.
  * Allows to retrieve the credentials during registration process and pass them into the destination controller method.
+ * Enforces validation of the password and username fields.
  *
  * @author Tomasz Lipka
  */
@@ -22,12 +25,12 @@ import java.util.Arrays;
 public class CustomUserDto {
 
     @NotNull
-//    @Size(min = 3, max = 10, message = "Username must have at least 3 and a maximum of 10 characters.")
+    @Size(min = 3, max = 10, message = "Username must have at least 3 and a maximum of 10 characters.")
     @NotBlank(message = "Username may not be blank")
     private String username;
 
     @NotNull
-//    @ValidPassword
+    @ValidPassword
     private char[] password;
 
     public void clearPassword() {
