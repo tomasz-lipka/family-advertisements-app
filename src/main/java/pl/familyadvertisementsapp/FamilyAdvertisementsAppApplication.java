c688b99ba884d53a0989fda7171e4bba8dfb31f2
Tomasz Lipka <>
2023-04-19 12:38:01 +0200

    Add jasypt encryption of database password

--- src/main/java/pl/familyadvertisementsapp/FamilyAdvertisementsAppApplication.java
+++ src/main/java/pl/familyadvertisementsapp/FamilyAdvertisementsAppApplication.java
@@ -1,9 +1,11 @@
 package pl.familyadvertisementsapp;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class FamilyAdvertisementsAppApplication {
 
     public static void main(String[] args) {
